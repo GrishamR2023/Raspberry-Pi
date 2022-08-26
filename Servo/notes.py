a6767647552d3d78aca9ecba419945dbc72fb3fd
rece grisham <rece.grisham@evsck12.com>
2022-08-25 22:07:46 -0500

    Notes Updated

--- Servo/notes.py
+++ Servo/notes.py
@@ -14,6 +14,24 @@ servo.start(0)
 # 7.5   = turned middle               (0,90)
 # 12.5  = completely turned left      (90/180)
 
-#Formula for servo movement in Degrees
-#x = degrees
-#(10.5/180 * x) + 2
+# Formula for servo movement in Degrees
+# x = degrees value
+# y = (10.5/180 * x) + 2
+# z = .1/60 * x  --- Formula for servo movement time in ms --- Servo movement speed is .1 sec/60 degrees
+# servo.ChangeDutyCycle(y)
+# time.sleep(z+.1)  ---  extra .1 considers for any errors or delays in servo runtime.
+
+#Function for formula
+def rotate(x):
+    y = (10.5/180 * x) + 2
+    z = .1/60 * x
+    servo.ChangeDutyCycle(y)
+    sleep(z)
+
+#Function for formula with user input
+def rotateInput():
+    x=input("Enter Degrees Value")
+    y = (10.5/180 * x) + 2
+    z = .1/60 * x
+    servo.ChangeDutyCycle(y)
+    sleep(z)
